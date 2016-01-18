57292dc865d85dc8ec5613da6ca104258f6230e0
liuxiaohang3342 <173050594@qq.com>
2016-01-18 18:13:33 +0800

    HorizontalScrollViewPager的修改

--- app/src/main/java/com/example/lxh/soso/customview/HorizontalScrollViewPager.java
+++ app/src/main/java/com/example/lxh/soso/customview/HorizontalScrollViewPager.java
@@ -13,7 +13,6 @@ import android.widget.Scroller;
 
 import com.example.lxh.soso.utils.ScreenUtils;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -28,9 +27,8 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
 
     private Context mContext;
 
-    private int mCurrentChildViewIndex;
+    private int mCurrentChildViewIndex = 1;
 
-    private int mTouchSlop;
     private Scroller mScroller;
 
     private float mActionDownX;
@@ -40,6 +38,11 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
 
     private ViewPageTransformer mTransformer;
 
+    private int mItemWidth; //每一个view的宽度
+
+    private int mMaxScrollX; //最大的偏移距离
+
+    private List<View> mChildViews;
 
     public HorizontalScrollViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -50,7 +53,7 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
 
     private void init(Context context) {
         mScreenWidth = ScreenUtils.getScreen(context)[0];
-        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        mItemWidth = mScreenWidth / 2;
         mScroller = new Scroller(context);
         ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         mContainer = new LinearLayout(context);
@@ -60,37 +63,48 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
         mTransformer = new ViewPageTransformer() {
             @Override
             public void transformPage(View page, float position) {
-                if (position < 0) {
-                    page.setScaleX(position + 1);
-                    page.setScaleY(position + 1);
+                if (position <= 0) {
+                    page.setScaleX(1 + position / 2);
+                    page.setScaleY(1 + position / 2);
                 } else if (position > 0) {
-                    page.setScaleX(1 - position);
-                    page.setScaleY(1 - position);
+                    page.setScaleX(1 - position / 2);
+                    page.setScaleY(1 - position / 2);
                 }
             }
         };
     }
 
-
-    public void setTransformer(ViewPageTransformer transformer) {
-        mTransformer = transformer;
-
+    public void setData(List<View> childViews) {
+        mChildViews = childViews;
+        notifyDataChange();
     }
 
-    public void setData(List<View> mChildViews) {
-        mContainer.removeAllViews();
+    /**
+     * 集合数据发生变化
+     */
+    public void notifyDataChange() {
         MAXCURRENTINDEX = mChildViews.size();
-        mCurrentChildViewIndex = 1;
+        createChildView(mChildViews);
+        mMaxScrollX = (mContainer.getChildCount() - 1) * mItemWidth - mScreenWidth;
+    }
+
+    /**
+     * 根据数据添加childview
+     *
+     * @param childViews
+     */
+    private void createChildView(List<View> childViews) {
+        mContainer.removeAllViews();
         LinearLayout.LayoutParams firstViewParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT);
         firstViewParams.width = mScreenWidth / 4;
         View firstview = new View(mContext);
         firstview.setLayoutParams(firstViewParams);
         mContainer.addView(firstview);
-        for (int i = 0; i < mChildViews.size(); i++) {
-            View childview = mChildViews.get(i);
+        for (int i = 0; i < childViews.size(); i++) {
+            View childview = childViews.get(i);
             if (childview != null) {
                 LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT);
-                layoutParams.width = mScreenWidth / 2;
+                layoutParams.width = mItemWidth;
                 childview.setLayoutParams(layoutParams);
                 mContainer.addView(childview);
             }
@@ -100,6 +114,32 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
         lastViewParams.width = mScreenWidth / 4;
         lastview.setLayoutParams(lastViewParams);
         mContainer.addView(lastview);
+    }
+
+
+    /**
+     * 设置transformer
+     *
+     * @param transformer
+     */
+    public void setTransformer(ViewPageTransformer transformer) {
+        mTransformer = transformer;
+
+    }
+
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if (mTransformer != null && mContainer != null) {
+            final int scrollX = getScrollX();
+            final int childCount = mContainer.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                final View child = mContainer.getChildAt(i);
+                final float transformPos = (float) (child.getLeft() - mScreenWidth / 4 - scrollX) / mItemWidth;
+                mTransformer.transformPage(child, transformPos);
+            }
+        }
     }
 
 
@@ -116,23 +156,33 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
                 return true;
             case MotionEvent.ACTION_MOVE:
                 float dx = x - mLastX;
-                if ((dx > 0 && getScrollX() == 0) || (dx < 0 && getScrollX() == 2 * mScreenWidth)) { //view并没有移动
+                scrollx = x - mActionDownX;
+                if ((dx > 0 && getScrollX() == 0) || (dx < 0 && getScrollX() == mMaxScrollX)) { //view并没有移动
                     mActionDownX = x;
+                    mLastX = x;
+                    return super.onTouchEvent(ev);
                 }
-                if (Math.abs(scrollx) >= mScreenWidth / 4) {
+                if (Math.abs(dx) >= mItemWidth) { //移动距离超过view的宽度，需要更新角标
                     updateCurrentViewIndex(scrollx);
+                    mLastX = x;
                 }
-                int offset = (int) scrollx % (mScreenWidth / 2);
-                float position = offset * 1f / (mScreenWidth / 2);
-//                Log.i("lxh", "offset" + offset);
-//                Log.i("lxh", "position" + position);
-//                callTransformer(position);
-                mLastX = x;
                 break;
             case MotionEvent.ACTION_UP:
                 scrollx = x - mActionDownX;
                 if (Math.abs(scrollx) >= mScreenWidth / 4) { //切换view
-                    updateCurrentViewIndex(scrollx);
+                    if (Math.abs(scrollx) < mItemWidth) {
+                        if (scrollx > 0) {
+                            mCurrentChildViewIndex--;
+                        } else {
+                            mCurrentChildViewIndex++;
+                        }
+                        if (mCurrentChildViewIndex < MINCURRENTINDEX) {
+                            mCurrentChildViewIndex = MINCURRENTINDEX;
+                        }
+                        if (mCurrentChildViewIndex > MAXCURRENTINDEX) {
+                            mCurrentChildViewIndex = MAXCURRENTINDEX;
+                        }
+                    }
                     int[] wh = new int[2];
                     mContainer.getChildAt(mCurrentChildViewIndex).getLocationOnScreen(wh);
                     int left = wh[0];
@@ -147,15 +197,20 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
         return super.onTouchEvent(ev);
     }
 
+    /**
+     * 更新焦点item的脚标
+     *
+     * @param dx
+     */
     private void updateCurrentViewIndex(float dx) {
         if (dx > 0) { //向右滑动，焦点index减一
             mCurrentChildViewIndex--;
-            if (Math.abs(dx) > mScreenWidth * 3 / 4) {//滑动的距离可以跳过一个view
+            if (Math.abs(dx) >= mScreenWidth * 3 / 4) {//滑动的距离可以跳过一个view
                 mCurrentChildViewIndex--;
             }
         } else {//向左滑动，焦点index加一
             mCurrentChildViewIndex++;
-            if (Math.abs(dx) > mScreenWidth * 3 / 4) {//滑动的距离可以跳过一个view
+            if (Math.abs(dx) >= mScreenWidth * 3 / 4) {//滑动的距离可以跳过一个view
                 mCurrentChildViewIndex++;
             }
         }
@@ -165,26 +220,6 @@ public class HorizontalScrollViewPager extends HorizontalScrollView {
         if (mCurrentChildViewIndex > MAXCURRENTINDEX) {
             mCurrentChildViewIndex = MAXCURRENTINDEX;
         }
-        Log.i("lxh","mCurrentChildViewIndex:::"+mCurrentChildViewIndex);
-    }
-
-    private void callTransformer(float position) {
-        //(0,-1) pre (-0.5 ,- 1)  cur ( 0,-0.5) next (0.5 ,0) 左滑
-        //(0,1) pre (-0.5 ,0)  cur ( 0,0.5) next (0.5 ,1) //右滑
-
-        if (mTransformer != null) {
-            if (mCurrentChildViewIndex - 1 >= MINCURRENTINDEX) {
-                View preView = mContainer.getChildAt(mCurrentChildViewIndex - 1);
-                mTransformer.transformPage(preView, (position - 1) / 2);
-            }
-            View currentView = mContainer.getChildAt(mCurrentChildViewIndex);
-            mTransformer.transformPage(currentView, position / 2);
-            if (mCurrentChildViewIndex + 1 <= MAXCURRENTINDEX) {
-                View preView = mContainer.getChildAt(mCurrentChildViewIndex + 1);
-                mTransformer.transformPage(preView, (1 + position) / 2);
-            }
-        }
-
     }
 
     @Override
